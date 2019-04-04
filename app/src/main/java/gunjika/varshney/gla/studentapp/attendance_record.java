15068080c79a019fca2351179c6fbac86bf73cd8
Gunjika <gunjikavrshn23@gmail.com>
2019-04-04 10:20:07 +0530

    add new attendance record activity

---
+++ app/src/main/java/gunjika/varshney/gla/studentapp/attendance_record.java
@@ -0,0 +1,13 @@
+package gunjika.varshney.gla.studentapp;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class attendance_record extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_attendance_record);
+    }
+}
