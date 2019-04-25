3a857cca8fda9d49283982d91e312b04ba9c8d9d
Gunjika <gunjikavrshn23@gmail.com>
2019-04-25 20:50:07 +0530

    add some views to the app

---
+++ app/src/main/java/gunjika/varshney/gla/studentapp/AttendRecord.java
@@ -0,0 +1,13 @@
+package gunjika.varshney.gla.studentapp;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class AttendRecord extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_attend_record);
+    }
+}
--- app/src/main/java/gunjika/varshney/gla/studentapp/MainActivity.java
+++ app/src/main/java/gunjika/varshney/gla/studentapp/MainActivity.java
@@ -2,12 +2,25 @@ package gunjika.varshney.gla.studentapp;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.CheckBox;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+    TextView textView,textView1;
+    Button button,button1;
+    CheckBox checkBox;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        textView=findViewById(R.id.editText);
+        textView1=findViewById(R.id.editText2);
+        button=findViewById(R.id.button);
+        button1=findViewById(R.id.button2);
+        checkBox=findViewById(R.id.checkBox);
+
+
     }
 }
